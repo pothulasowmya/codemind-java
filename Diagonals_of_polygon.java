2054f48d11af0e5965a2eccb08b132534291e32b
pothulasowmya <84896000+pothulasowmya@users.noreply.github.com>
2022-09-22 20:46:43 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,d;      n=sc.nextInt();     d=n*(n-3)/2;     System.out.println(d);
        }
    
    }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,d;
+     n=sc.nextInt();
+    d=n*(n-3)/2;
+    System.out.println(d);
+        
+    }
+
+}
