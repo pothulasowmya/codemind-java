bf12c47ba96653d135503b9bf4fb131f81c4cedf
pothulasowmya <84896000+pothulasowmya@users.noreply.github.com>
2022-09-22 14:32:28 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,k,c=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)           x[i]=sc.nextInt();         k=sc.nextInt();         for(i=0;i<n;i++)         {             if(x[i]==k)             c++;         }         System.out.println(c);     } }

--- count_of_given_element_in_an_array.java
+++ count_of_given_element_in_an_array.java
@@ -1,27 +1,20 @@
 import java.util.Scanner;
 class Sample
 {
- public static void main(String args[])
- {
-   Scanner sc=new Scanner(System.in);
-   int n,x[],i,k,count=0;
-   n=sc.nextInt();
-    x=new int[n];
-    for(i=0;i<n;i++)
+    public static void main(String args[])
     {
-     x[i]=sc.nextInt();
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i,k,c=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+          x[i]=sc.nextInt();
+        k=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(x[i]==k)
+            c++;
+        }
+        System.out.println(c);
     }
-     k=sc.nextInt();
-     for(i=0;i<n;i++)
-    {
-      if(x[i]==k)
-      {
-       count++;
-      }
-    }
-
-     System.out.println(count);
-
-  }
 }
-
