bfcf06148820cb650a14f3c1e3051d9d44baf829
pothulasowmya <84896000+pothulasowmya@users.noreply.github.com>
2022-09-25 18:56:32 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         int n,x[],i,s=0,c=0;         float a;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)            x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             s+=x[i];         }         a=(s/n);         for(i=0;i<n;i++)         {             if(x[i]<=a)             c++;         }         System.out.println(c);     } }

---
+++ elements_smaller_than_average.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String args[])
+    {
+        int n,x[],i,s=0,c=0;
+        float a;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+           x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            s+=x[i];
+        }
+        a=(s/n);
+        for(i=0;i<n;i++)
+        {
+            if(x[i]<=a)
+            c++;
+        }
+        System.out.println(c);
+    }
+}
