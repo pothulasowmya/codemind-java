ffe6eab9396f41967ac4eeca0e95d4136e306555
pothulasowmya <84896000+pothulasowmya@users.noreply.github.com>
2022-10-16 08:30:36 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[]) 	{     	int n,x[],i,c=0;     	float avg;     	Scanner sc=new Scanner(System.in);     	n=sc.nextInt();     	x=new int[n];     	for(i=0;i<n;i++)     	  x[i]=sc.nextInt();     	for(i=0;i<n;i++)     	{     	    c+=x[i];     	}     	avg=(int)c/n;     	for(i=0;i<n;i++)     	{     	    c=x[i];     	    if(x[i]==avg)     	    {     	        c=x[i];     	        break;     	    }     	}     	if(avg==c)     	  System.out.println("True");     	else     	  System.out.println("False"); 	} }

--- average_element_is_in_array_or_not.java
+++ average_element_is_in_array_or_not.java
@@ -1,33 +1,32 @@
 import java.util.Scanner;
 class Sample
 {
- public static void main(String args[])
- {
-   Scanner sc=new Scanner(System.in);
-   int n,x[],i,s=0,count=0;
-   float avg;
-   n=sc.nextInt();
-    x=new int[n];
-    for(i=0;i<n;i++)
-    {
-     x[i]=sc.nextInt();
-     s=s+x[i];
-    }
-    avg=s/n;
-     for(i=0;i<n;i++)
-    {
-      if(x[i]==avg)
-      {
-       count++;
-      }
-    }
-    if(count>0)
-    {
-        System.out.println("True");
-    }
-    else
-    {
-        System.out.println("False");
-    }
-  }
+    public static void main(String args[])
+	{
+    	int n,x[],i,c=0;
+    	float avg;
+    	Scanner sc=new Scanner(System.in);
+    	n=sc.nextInt();
+    	x=new int[n];
+    	for(i=0;i<n;i++)
+    	  x[i]=sc.nextInt();
+    	for(i=0;i<n;i++)
+    	{
+    	    c+=x[i];
+    	}
+    	avg=(int)c/n;
+    	for(i=0;i<n;i++)
+    	{
+    	    c=x[i];
+    	    if(x[i]==avg)
+    	    {
+    	        c=x[i];
+    	        break;
+    	    }
+    	}
+    	if(avg==c)
+    	  System.out.println("True");
+    	else
+    	  System.out.println("False");
+	}
 }
