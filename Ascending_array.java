aeed49ece1e10ffd35c20d0ab3a1bf43155b5074
pothulasowmya <84896000+pothulasowmya@users.noreply.github.com>
2022-10-22 20:42:12 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         int n,x[];         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(int i=0;i<n-1;i++)         {             if(x[i]>=x[i+1])             {                 System.out.println("no");                 System.exit(0);             }         }         System.out.println("yes");         System.exit(0);     } }

--- Ascending_array.java
+++ Ascending_array.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 class Sample
 {
- public static void main(String args[])
- {
-   Scanner sc=new Scanner(System.in);
-   int n,x[],i;
-   n=sc.nextInt();
-   x=new int[n];
-   for(i=0;i<n;i++)
-     x[i]=sc.nextInt();
-  for(i=0;i<n-1;i++)
-  {
-    if(x[i]>=x[i+1])
+    public static void main(String args[])
     {
-      System.out.println("no");
-      System.exit(0);
+        int n,x[];
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            if(x[i]>=x[i+1])
+            {
+                System.out.println("no");
+                System.exit(0);
+            }
+        }
+        System.out.println("yes");
+        System.exit(0);
     }
-  }
-  System.out.println("yes");
-  
-}
 }
