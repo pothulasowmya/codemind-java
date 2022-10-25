0550e8a718d049b0baf96ed72e3cdd3fb0defa57
pothulasowmya <84896000+pothulasowmya@users.noreply.github.com>
2022-10-25 20:30:52 +0530

    import java.util.Scanner; class Sample {     public static boolean isprime(int n)     {         if(n==1)          return false;         int count=0;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             count++;         }         if(count==0)            return true;         else            return false;     }     public static void main(String args[])     {         int n,x[],c=0,i,a;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)            x[i]=sc.nextInt();         a=sc.nextInt();         for(i=0;i<n;i++)         {             if(x[i]<=a)             {                 if(isprime(x[i]))                 c++;             }         }         System.out.println(c);     } }

--- primes_less_than_k.java
+++ primes_less_than_k.java
@@ -1,38 +1,38 @@
 import java.util.Scanner;
 class Sample
 {
-    public static boolean isPrime(int n)
+    public static boolean isprime(int n)
     {
-        if(n<2)
-          return false;
+        if(n==1)
+         return false;
         int count=0;
-    for(int i=2;i<=(int)Math.sqrt(n);i++)
-    {
-        if(n%i==0)
-           count++;
-    }
-    if(count==0)
-      return true;
-    else
-      return false;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            count++;
+        }
+        if(count==0)
+           return true;
+        else
+           return false;
     }
     public static void main(String args[])
     {
-        int n,x[],k,count=0,i;
+        int n,x[],c=0,i,a;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
-          x[i]=sc.nextInt();
-        k=sc.nextInt();
+           x[i]=sc.nextInt();
+        a=sc.nextInt();
         for(i=0;i<n;i++)
         {
-            if(isPrime(x[i]))
+            if(x[i]<=a)
             {
-                if(x[i]<=k)
-                  count++;
+                if(isprime(x[i]))
+                c++;
             }
         }
-        System.out.println(count);
+        System.out.println(c);
     }
 }
