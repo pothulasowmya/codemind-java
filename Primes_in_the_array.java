f430a75449eec7edcd592207cabaf834ae0c9a51
pothulasowmya <84896000+pothulasowmya@users.noreply.github.com>
2022-10-25 20:39:30 +0530

    import java.util.Scanner; class Sample {     public static boolean isprime(int n)     {         if(n==1)          return false;         int count=0;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             count++;         }         if(count==0)            return true;         else            return false;     }     public static void main(String args[])     {         int n,x[],c=0,i;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)            x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             if(isprime(x[i]))                  c++;
            }
            System.out.println(c);
        }
    }

--- Primes_in_the_array.java
+++ Primes_in_the_array.java
@@ -1,41 +1,35 @@
 import java.util.Scanner;
 class Sample
 {
-    public static boolean isPrime(int x)
+    public static boolean isprime(int n)
     {
-        if(x<2)
-        return false;
+        if(n==1)
+         return false;
         int count=0;
-        for(int i=2;i<=(int)Math.sqrt(x);i++)
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
         {
-          if(x%i==0)
-          {
-           count++;
-          }
+            if(n%i==0)
+            count++;
         }
         if(count==0)
-        {
-         return true;
-        }
-        else 
-        {
-          return false;
-        }
+           return true;
+        else
+           return false;
     }
     public static void main(String args[])
     {
-      Scanner sc =new Scanner(System.in);
-      int n,x[],c=0,i;
-       n=sc.nextInt();
-       x=new int[n];
-      for(i=0;i<n;i++)
-      {
-        x[i]=sc.nextInt();
-        if(isPrime(x[i]))
+        int n,x[],c=0,i;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+           x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
         {
-         c++;
+            if(isprime(x[i]))
+                 c++;
+            
         }
-      }
-   System.out.println(c);
-}
+        System.out.println(c);
+    }
 }
