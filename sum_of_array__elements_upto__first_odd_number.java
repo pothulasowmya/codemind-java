e7f635b69aabbe293d4680965898c4632dbc1d5b
pothulasowmya <84896000+pothulasowmya@users.noreply.github.com>
2022-10-25 20:57:34 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         int n,x[],i,s=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)            x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             if(x[i]%2!=0)             break;             s+=x[i];         }         System.out.println(s);     } }

---
+++ sum_of_array__elements_upto__first_odd_number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        int n,x[],i,s=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+           x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(x[i]%2!=0)
+            break;
+            s+=x[i];
+        }
+        System.out.println(s);
+    }
+}
