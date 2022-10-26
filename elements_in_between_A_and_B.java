bd4e5d840524a1172e138580c76de3c4c9d67c2e
pothulasowmya <84896000+pothulasowmya@users.noreply.github.com>
2022-10-26 20:52:24 +0530

    import java.util.Scanner; class Sample {     public static void main(String arg[])     {         int n,x[],a,b,i,c=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)            x[i]=sc.nextInt();         a=sc.nextInt();         b=sc.nextInt();         for(i=0;i<n;i++)         {             if(x[i]>=a&&x[i]<=b)             {                 System.out.print(x[i]+" ");                 c++;             }         }         if(c==0)         System.out.println("-1");     } }

--- elements_in_between_A_and_B.java
+++ elements_in_between_A_and_B.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 class Sample
 {
-    public static void main(String args[])
+    public static void main(String arg[])
     {
-        int n,x[],c=0,b,a;
+        int n,x[],a,b,i,c=0;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
         x=new int[n];
-        for(int i=0;i<n;i++)
-            x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+           x[i]=sc.nextInt();
         a=sc.nextInt();
         b=sc.nextInt();
-        for(int i=0;i<n;i++)
+        for(i=0;i<n;i++)
         {
-            if((x[i]>=a) && (x[i]<=b))
+            if(x[i]>=a&&x[i]<=b)
             {
-              System.out.print(x[i]+" ");
-              c++;
+                System.out.print(x[i]+" ");
+                c++;
             }
         }
         if(c==0)
-           System.out.println(-1);
+        System.out.println("-1");
     }
-    
 }
