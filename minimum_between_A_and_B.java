fa2c99e7d62210fb2bda19016debce1e68fb55cf
pothulasowmya <84896000+pothulasowmya@users.noreply.github.com>
2022-10-26 20:28:30 +0530

    import java.util.Scanner; class Sample {     public static void main(String arg[])     {         int n,x[],a,b,i,c=0,s;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)            x[i]=sc.nextInt();         a=sc.nextInt();         b=sc.nextInt();         s=0;         for(i=0;i<n;i++)         {             if(s<x[i])             s=x[i];         }         for(i=0;i<n;i++)         {             if(x[i]>=a&&x[i]<=b)             {                 if(s>x[i])                 {                     s=x[i];                     c++;                 }             }         }         if(c!=0)         System.out.println(s);         else         System.out.println("-1");     } }

--- minimum_between_A_and_B.java
+++ minimum_between_A_and_B.java
@@ -1,33 +1,36 @@
 import java.util.Scanner;
 class Sample
 {
-    public static void main(String args[])
+    public static void main(String arg[])
     {
-        int n,x[],s=0,r=0,c=0,b,a;
+        int n,x[],a,b,i,c=0,s;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
         x=new int[n];
-        for(int i=0;i<n;i++)
-            x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+           x[i]=sc.nextInt();
         a=sc.nextInt();
         b=sc.nextInt();
-        r=x[n-1];
-        for(int i=0;i<n;i++)
+        s=0;
+        for(i=0;i<n;i++)
         {
-            if((x[i]>=a) && (x[i]<=b))
+            if(s<x[i])
+            s=x[i];
+        }
+        for(i=0;i<n;i++)
+        {
+            if(x[i]>=a&&x[i]<=b)
             {
-                if(r>x[i])
+                if(s>x[i])
                 {
-                    r=x[i];
+                    s=x[i];
                     c++;
                 }
-                
             }
         }
-        if(c>0)
-           System.out.println(r);
+        if(c!=0)
+        System.out.println(s);
         else
-            System.out.println("-1");
+        System.out.println("-1");
     }
-    
 }
