e2d718ee198ad7f7a0bd3a7bb14d33c5c2435ef2
pothulasowmya <84896000+pothulasowmya@users.noreply.github.com>
2023-01-29 11:54:11 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         int n,x[],c=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         x[i]=sc.nextInt();         for(int i=0;i<n-2;i++)         {             if((x[i]%2!=0) && (x[i+2]%2!=0))             if(x[i+1]%2!=0)             {                 c++;             }         }         System.out.println(c);     } }

--- count_the_add_numbers_inbetween_odd_numbers.java
+++ count_the_add_numbers_inbetween_odd_numbers.java
@@ -3,18 +3,20 @@ class Sample
 {
     public static void main(String args[])
     {
-        int n,x[],i,count=0;
+        int n,x[],c=0;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
         x=new int[n];
-        for(i=0;i<n;i++)
-           x[i]=sc.nextInt();
-        for(i=0;i<n-2;i++)
+        for(int i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        for(int i=0;i<n-2;i++)
         {
-            if(x[i]%2!=0 && x[i+2]%2!=0)
-               if(x[i+1]%2!=0)
-                count++;
+            if((x[i]%2!=0) && (x[i+2]%2!=0))
+            if(x[i+1]%2!=0)
+            {
+                c++;
+            }
         }
-        System.out.println(count);
+        System.out.println(c);
     }
 }
